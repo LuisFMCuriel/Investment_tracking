594a250cb3df8e4a64e7779f92506b64fcf30672
Luis Felipe Morales Curiel <luisfelipemoralescuriel@luiss-mac-mini-2.home>
2026-03-28 22:18:55 +0100

    added rest pnl

--- app/main.py
+++ app/main.py
@@ -4,6 +4,7 @@ from app.core.config import settings
 from app.api.portfolio import router as portfolio_router
 from app.api.health import router as health_router
 from app.api.positions import router as positions_router
+from app.api.pnl import router as pnl_router
 
 app = FastAPI(
     title=settings.app_name,
@@ -12,8 +13,9 @@ app = FastAPI(
 
 @app.get("/")
 def root() -> dict[str, str]:
-    return {"message": "AI Portfolio Tracker API, hi"}
+    return {"message": "AI Portfolio Tracker API"}
 
 app.include_router(health_router)
 app.include_router(portfolio_router)
 app.include_router(positions_router)
+app.include_router(pnl_router)
